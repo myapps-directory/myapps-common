12658dacf9315803efab4d8e4ab3ac58e8f5b312
Valentin Palade <vipalade@gmail.com>
2019-01-14 15:24:15 +0200

    update front protocol

--- ola_front_protocol.hpp
+++ ola_front_protocol.hpp
@@ -82,6 +82,50 @@ struct ListOSesResponse : solid::frame::mprpc::Message {
     }
 };
 
+struct GenericResponse : solid::frame::mprpc::Message {
+    uint32_t    error_;
+    std::string message_;
+
+    GenericResponse() {}
+
+    GenericResponse(
+        const solid::frame::mprpc::Message& _rreq)
+        : solid::frame::mprpc::Message(_rreq)
+        , error_(-1)
+    {
+    }
+
+    SOLID_PROTOCOL_V2(_s, _rthis, _rctx, _name)
+    {
+        _s.add(_rthis.error_, _rctx, "error").add(_rthis.message_, _rctx, "message");
+    }
+};
+
+struct AppConfig{
+    using KVVectorT = std::vector<std::pair<std::string, std::string>>;
+    std::string name_;
+    std::string short_description_;
+    std::string description_;
+    KVVectorT   name_vec_;
+    
+    SOLID_PROTOCOL_V2(_s, _rthis, _rctx, _name)
+    {
+        _s.add(_rthis.name_, _rctx, "name");
+        _s.add(_rthis.short_description_, _rctx, "short_description");
+        _s.add(_rthis.description_, _rctx, "description");
+        _s.add(_rthis.name_vec_, _rctx, "name_vec");
+    }
+};
+
+struct CreateAppRequest: solid::frame::mprpc::Message{
+    AppConfig config_;
+    
+    SOLID_PROTOCOL_V2(_s, _rthis, _rctx, _name)
+    {
+        _s.add(_rthis.config_, _rctx, "config");
+    }
+};
+
 using ProtocolT = solid::frame::mprpc::serialization_v2::Protocol<uint8_t>;
 
 template <class R>
@@ -91,9 +135,12 @@ inline void protocol_setup(R _r, ProtocolT& _rproto)
 
     _r(_rproto, solid::TypeToType<AuthRequest>(), 1);
     _r(_rproto, solid::TypeToType<AuthResponse>(), 2);
+    _r(_rproto, solid::TypeToType<GenericResponse>(), 3);
 
     _r(_rproto, solid::TypeToType<ListOSesRequest>(), 10);
     _r(_rproto, solid::TypeToType<ListOSesResponse>(), 11);
+    
+    _r(_rproto, solid::TypeToType<CreateAppRequest>(), 21);
 }
 
 } //namespace front
